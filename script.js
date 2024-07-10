f60819ab0c3f94a8471cd4166dacce6f73bf7aef
shahzaib <shezi11221@gmail.com>
2024-07-11 00:19:57 +0500

    completed

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// for javascript editor
+
+function outputPanel(){
+    $("iframe").contents().find("html").html("<html><head><style>" + $("#cssPanel").val() +"</style></head><body>" + $("#htmlPanel").val() +"</body></html>");
+    document.getElementById("outputPanel").contentWindow.eval($("#jsPanel").val());
+}
+
 $(".toggleButton").hover(function(){
     $(this).addClass("highlight");
 },function(){
@@ -7,6 +14,13 @@ $(".toggleButton").hover(function(){
 
 $(".toggleButton").click(function(){
     $(this).toggleClass("active");
+    $(this).removeClass("highlight");
+    var panelID = $(this).attr("id") + "Panel";
+    $("#" + panelID).toggleClass("hidden");
+
+    var activePanels = 4 - $(".hidden").length;
+    $(".panel").width(($(window).width() / activePanels) - 35)
+    
 })
 
 
@@ -14,4 +28,8 @@ $(".toggleButton").click(function(){
 
 $(".panel").height($(window).height() -100)
 
-$(".panel").width(($(window).width() / 3) - 35)
+outputPanel();
+
+$("textarea").on("change keyup paste", function(){
+    outputPanel();
+})
